dba78802d22fbc5141579b224cc54704507849a6
Iain Mathieson <iain.mathieson@cantab.net>
2014-11-04 01:29:13 -0500

    modify inference_lattice to work with k1 and k2

--- inference_lattice.R
+++ inference_lattice.R
@@ -28,13 +28,14 @@ EPSILON <- 10e-100                       #Used as a minimum value in some calcul
 
 s.m.lattice.likelihood <- function(s, m, N, fr, h=0.5){
   g <- dim(fr)[3]
-  k <- dim(fr)[1]
+  k1 <- dim(fr)[1]
+  k2 <- dim(fr)[2]
 
-  if(!is.matrix(s)==1){s=matrix(s, nrow=k, ncol=k)}
-  mrm <- mig.rate.mat(k)
+  if(!is.matrix(s)==1){s=matrix(s, nrow=k1, ncol=k2)}
+  mrm <- mig.rate.mat(k1,k2)
 
-  mu <- array(0, c(k,k,g))
-  s.2 <- array(0,c(k,k,g))
+  mu <- array(0, c(k1,k2,g))
+  s.2 <- array(0,c(k1,k2,g))
 
   hi <- fr[,,t-1]+h*(1-2*fr[,,t-1])
   
@@ -44,26 +45,26 @@ s.m.lattice.likelihood <- function(s, m, N, fr, h=0.5){
     s.2[,,t] <- fr[,,t-1]*(1-fr[,,t-1])/N
 
     ## Sorry!
-    for(i in 2:k){
-      for( j in 1:k){
+    for(i in 2:k1){
+      for( j in 1:k2){
         mu[i,j,t]=mu[i,j,t]+(m/N)*fr[i-1,j,t-1]
       }
     }
 
-    for(i in 1:(k-1)){
-      for( j in 1:k){
+    for(i in 1:(k1-1)){
+      for( j in 1:k2){
         mu[i,j,t]=mu[i,j,t]+(m/N)*fr[i+1,j,t-1]
       }
 
     }
-    for(i in 1:k){
-      for( j in 2:k){
+    for(i in 1:k1){
+      for( j in 2:k2){
         mu[i,j,t]=mu[i,j,t]+(m/N)*fr[i,j-1,t-1]
       }
     }
 
-    for(i in 1:k){
-      for( j in 1:(k-1)){
+    for(i in 1:k1){
+      for( j in 1:(k2-1)){
         mu[i,j,t]=mu[i,j,t]+(m/N)*fr[i,j+1,t-1]
       }
     }
@@ -83,19 +84,22 @@ s.m.lattice.likelihood <- function(s, m, N, fr, h=0.5){
 ## wrapper to be used for numerical optimization
 ## m is known (returns negative ll)
 s.lattice.likelihood.vector <- function(s, m, N, fr, h=0.5){
-  k <- dim(fr)[1]
-  s <- matrix(s, nrow=k, ncol=k, byrow=TRUE)
+  k1 <- dim(fr)[1]
+  k2 <- dim(fr)[2]
+
+  s <- matrix(s, nrow=k1, ncol=k2, byrow=TRUE)
   return(-s.m.lattice.likelihood(s, m, N, fr, h=h))
 }
 
 ## wrapper for numerical optimisation
 ## m is unknown. The first vector should of length
-## 2 or k*k+1 with the last element being m
+## 2 or k1*k2+1 with the last element being m
 s.m.lattice.likelihood.vector <- function(s.m, N, fr, h=0.5){
-  k <- dim(fr)[1]
+  k1 <- dim(fr)[1]
+  k2 <- dim(fr)[2]
   m <- s.m[length(s.m)]
   s <- s.m[1:(length(s.m)-1)]
-  s <- matrix(s, nrow=k, ncol=k, byrow=TRUE)
+  s <- matrix(s, nrow=k1, ncol=k2, byrow=TRUE)
   return(-s.m.lattice.likelihood(s, m, N, fr, h=h))
 }  
 
@@ -106,9 +110,10 @@ m.lattice.likelihood <- function(m, s, N, fr, h=0.5){
 
 ## Numerical MLE for s in the lattice
 lattice.s.hat <- function(fr, m, N, h=0.5){
-  k <- dim(fr)[1]
-  result <- optim(rep(0.05,k*k), s.lattice.likelihood.vector, m=m, N=N, fr=fr, h=h)$par
-  s.hat <- matrix(result, nrow=k, byrow=TRUE)
+  k1 <- dim(fr)[1]
+  k2 <- dim(fr)[2]
+  result <- optim(rep(0.05,k1*k2), s.lattice.likelihood.vector, m=m, N=N, fr=fr, h=h)$par
+  s.hat <- matrix(result, nrow=k1, byrow=TRUE)
   return(s.hat)
 }  
 
@@ -116,11 +121,13 @@ lattice.s.hat <- function(fr, m, N, h=0.5){
 ## in a lattice model (M=m/N)
 
 approx.lattice.s.hat <- function(f, M, h=0.5){
-  k <- dim(f)[1]
-  s.hat <- matrix(0,nrow=k,ncol=k)
+  k1 <- dim(f)[1]
+  k2 <- dim(f)[2]
 
-  for(i in 1:k){
-    for(j in 1:k){
+  s.hat <- matrix(0,nrow=k1,ncol=k2)
+
+  for(i in 1:k1){
+    for(j in 1:k2){
       s.hat[i,j] <- approx.lattice.s.hat.ij(i,j,f,M, h=h)
     }
   }
@@ -128,18 +135,20 @@ approx.lattice.s.hat <- function(f, M, h=0.5){
 }
 
 approx.lattice.s.hat.ij <- function(i,j,f,M, h=0.5){
-  k <- dim(f)[1]
+  k1 <- dim(f)[1]
+  k2 <- dim(f)[2]
+
   G <- dim(f)[3]-1
-  mrm <- mig.rate.mat(k)
+  mrm <- mig.rate.mat(k1, k2)
 
   fi <- f[i,j,1:G]
   hi <- fi+h*(1-2*fi)
 
   term1 <- approx.MLE(f[i,j,], h=h)
   term2 <- mrm[i,j]*sum(hi*f[i,j,1:G])
-  if(i<k){term2=term2-sum(hi*f[i+1,j,1:G])}
+  if(i<k1){term2=term2-sum(hi*f[i+1,j,1:G])}
   if(i>1){term2=term2-sum(hi*f[i-1,j,1:G])}
-  if(j<k){term2=term2-sum(hi*f[i,j+1,1:G])}
+  if(j<k2){term2=term2-sum(hi*f[i,j+1,1:G])}
   if(j>1){term2=term2-sum(hi*f[i,j-1,1:G])}
   
   het <- sum(2*hi*hi*f[i,j,1:G]*(1-f[i,j,1:G]))
@@ -155,18 +164,19 @@ approx.lattice.s.hat.ij <- function(i,j,f,M, h=0.5){
 ## MLE for s, assuming that it is constant
 approx.lattice.s.hat.constant <- function(f, M, h=0.5){
 ##   if(h!=0.5){stop("non-genic selection not implemented for constant s")}
-  k <- dim(f)[1]
+  k1 <- dim(f)[1]
+  k2 <- dim(f)[2]
   G <- dim(f)[3]-1
   ht <- f+h*(1-2*f)
   
-  mrm <- mig.rate.mat(k)
+  mrm <- mig.rate.mat(k1, k2)
 
-  term1 <- matrix(0,k,k)                #1-m|k|
-  term2 <- matrix(0,k,k)                #f_G - f_0 + sum(m|k|fij - sum(fi'j'))
-  term3 <- matrix(0,k,k)                #f(1-f)
+  term1 <- matrix(0,k1,k2)                #1-m|k|
+  term2 <- matrix(0,k1,k2)                #f_G - f_0 + sum(m|k|fij - sum(fi'j'))
+  term3 <- matrix(0,k1,k2)                #f(1-f)
 
-  for( i in 1:k){
-    for(j in 1:k){
+  for( i in 1:k1){
+    for(j in 1:k2){
       term2[i,j] <- h*(f[i,j,G+1]-f[i,j,1])
       term3[i,j] <- sum(ht[i,j,1:G]*ht[i,j,1:G]*f[i,j,1:G]*(1-f[i,j,1:G]))
       if(h!=0.5){
@@ -175,23 +185,24 @@ approx.lattice.s.hat.constant <- function(f, M, h=0.5){
     }
   }
 
-  return(matrix(sum(term2)/sum(term3)/2,k,k))
+  return(matrix(sum(term2)/sum(term3)/2,k1,k2))
 }
 
 ## EM MLE for s, assuming that it is constant
 approx.lattice.s.hat.constant.em <- function(call, M, h=0.5){
   fb <- call$fb$fb.mat
-  k <- dim(fb)[1]
+  k1 <- dim(fb)[1]
+  k2 <- dim(fb)[2]
   G <- dim(fb)[4]-1
   D <- dim(fb)[3]
   states <- call$params$states
   ht <- states+h*(1-2*states)
   
-  term2 <- matrix(0,k,k)                #f_G - f_0 + sum(m|k|fij - sum(fi'j'))
-  term3 <- matrix(0,k,k)                #f(1-f)
+  term2 <- matrix(0,k1,k2)                #f_G - f_0 + sum(m|k|fij - sum(fi'j'))
+  term3 <- matrix(0,k1,k2)                #f(1-f)
 
-  for( i in 1:k){
-    for(j in 1:k){
+  for( i in 1:k1){
+    for(j in 1:k2){
       term2[i,j] <- h*(sum(fb[i,j,,G+1]*states)-sum(fb[i,j,,1]*states))      
       term3[i,j] <-sum(apply(fb[i,j,,1:G], 2, '*', ht*ht*states*(1-states)))
       if(h!=0.5){
@@ -213,18 +224,19 @@ approx.lattice.s.hat.constant.em <- function(call, M, h=0.5){
       }
     }
   }
-  return(matrix(sum(term2)/sum(term3)/2,k,k))
+  return(matrix(sum(term2)/sum(term3)/2,k1,k2))
 }
 
 ## approximate MLE for s, given M
 ## in a lattice model (M=m/N)
 
 approx.lattice.s.hat.em <- function(call, M, h=0.5){
-  k <- dim(call$fb$fb.mat)[1]
-  s.hat <- matrix(0,nrow=k,ncol=k)
+  k1 <- dim(call$fb$fb.mat)[1]
+  k2 <- dim(call$fb$fb.mat)[2]
+  s.hat <- matrix(0,nrow=k1,ncol=k2)
 
-  for(i in 1:k){
-    for(j in 1:k){
+  for(i in 1:k1){
+    for(j in 1:k2){
       s.hat[i,j] <- approx.lattice.s.hat.ij.em(i,j,call,M, h=h)
     }
   }
@@ -232,12 +244,10 @@ approx.lattice.s.hat.em <- function(call, M, h=0.5){
 }
 
 approx.lattice.s.hat.ij.em <- function(i,j,call,M, h=0.5){
-  ## if(h!=0.5){stop("Not supported")}
-
-  k <- dim(call$fb$fb.mat)[1]
-  mrm <- mig.rate.mat(k)
+  k1 <- dim(call$fb$fb.mat)[1]
+  k2 <- dim(call$fb$fb.mat)[2]
+  mrm <- mig.rate.mat(k1, k2)
   fb <- call$fb$fb.mat
-  k <- dim(fb)[1]
   G <- dim(fb)[4]-1
   states <- call$params$states
 
@@ -253,9 +263,9 @@ approx.lattice.s.hat.ij.em <- function(i,j,call,M, h=0.5){
   term2 <- mrm[i,j]*sum(apply(fb[i,j,,(1:G)], 2, '*', ht*states))
 
   exp.ht <- colSums(apply(fb[i,j,,(1:G)], 2, '*', ht))
-  if(i<k){term2=term2-sum(exp.ht*f[i+1,j,(1:G)])}
+  if(i<k1){term2=term2-sum(exp.ht*f[i+1,j,(1:G)])}
   if(i>1){term2=term2-sum(exp.ht*f[i-1,j,(1:G)])}
-  if(j<k){term2=term2-sum(exp.ht*f[i,j+1,(1:G)])}
+  if(j<k2){term2=term2-sum(exp.ht*f[i,j+1,(1:G)])}
   if(j>1){term2=term2-sum(exp.ht*f[i,j-1,(1:G)])}
 
   het <- sum(apply(fb[i,j,,1:G], 2, '*', ht*ht*states*(1-states))) #expected heterozygosity  
@@ -270,26 +280,27 @@ approx.lattice.s.hat.ij.em <- function(i,j,call,M, h=0.5){
 
 ## approximate MLE for m
 approx.lattice.m.hat <- function(f,s, h=0.5){
-  k <- dim(f)[1]
+  k1 <- dim(f)[1]
+  k2 <- dim(f)[2]
   G <- dim(f)[3]
-  mrm <- mig.rate.mat(k)
-  if(!is.matrix(s)){s <- matrix(s,k,k)}
+  mrm <- mig.rate.mat(k1, k2)
+  if(!is.matrix(s)){s <- matrix(s,k1,k2)}
 
-  term1 <- array(0, dim=c(k,k,G-1))     #f_{ij}^t-\mu_{ij}^t
-  term2 <- array(0, dim=c(k,k,G-1))     #|k|\mu_{ij}^t-\sum \mu_{i'j'}
-  term3 <- array(0, dim=c(k,k,G-1))     #f(1-f)
+  term1 <- array(0, dim=c(k1,k2,G-1))     #f_{ij}^t-\mu_{ij}^t
+  term2 <- array(0, dim=c(k1,k2,G-1))     #|k|\mu_{ij}^t-\sum \mu_{i'j'}
+  term3 <- array(0, dim=c(k1,k2,G-1))     #f(1-f)
 
   fij <- f[,,1:(G-1)]
   hij <- fij+h*(1-2*fij)
   
   het <- f[,,1:(G-1)]*(1-f[,,1:(G-1)])
-  term1 <- f[,,2:G]-f[,,1:(G-1)]-array(apply(het,3,'*',s),dim=c(k,k,G-1))*hij*2
-  term2 <- array(apply(f[,,1:(G-1)],3,'*',mrm),dim=c(k,k,G-1))
-  for(i in 1:k){
-    for(j in 1:k){
-      if(i<k){term2[i,j,]=term2[i,j,]-f[i+1,j,1:(G-1)]}
+  term1 <- f[,,2:G]-f[,,1:(G-1)]-array(apply(het,3,'*',s),dim=c(k1,k2,G-1))*hij*2
+  term2 <- array(apply(f[,,1:(G-1)],3,'*',mrm),dim=c(k1,k2,G-1))
+  for(i in 1:k1){
+    for(j in 1:k2){
+      if(i<k1){term2[i,j,]=term2[i,j,]-f[i+1,j,1:(G-1)]}
       if(i>1){term2[i,j,]=term2[i,j,]-f[i-1,j,1:(G-1)]}
-      if(j<k){term2[i,j,]=term2[i,j,]-f[i,j+1,1:(G-1)]}
+      if(j<k2){term2[i,j,]=term2[i,j,]-f[i,j+1,1:(G-1)]}
       if(j>1){term2[i,j,]=term2[i,j,]-f[i,j-1,1:(G-1)]}
     }
   }
@@ -305,27 +316,27 @@ approx.lattice.m.hat <- function(f,s, h=0.5){
 
 ## The em version of the MLE for m
 approx.lattice.m.hat.em <- function(call, h=0.5){
-  k <- dim(call$fb$fb.mat)[1]
-  mrm <- mig.rate.mat(k)
+  k1 <- dim(call$fb$fb.mat)[1]
+  k2 <- dim(call$fb$fb.mat)[2]
+  mrm <- mig.rate.mat(k1, k2)
   fb <- call$fb$fb.mat
-  k <- dim(fb)[1]
   G <- dim(fb)[4]-1
   states <- call$params$states
   ht <- states+h*(1-2*states)
   s <- call$params$s
   
-  term1 <- array(0,dim=c(k,k,G))
-  term2 <- array(0,dim=c(k,k,G))
+  term1 <- array(0,dim=c(k1,k2,G))
+  term2 <- array(0,dim=c(k1,k2,G))
 
   C <- states*(1-states)
-  for(i in 1:k){
-    for(j in 1:k){
+  for(i in 1:k1){
+    for(j in 1:k2){
       A <- states+2*h*s[i,j]*states*(1-states)
       for(t in 1:G){
         B <- states*mrm[i,j]
-        if(i<k){B <- B-sum(states*fb[i+1,j,,t])}
+        if(i<k1){B <- B-sum(states*fb[i+1,j,,t])}
         if(i>1){B <- B-sum(states*fb[i-1,j,,t])}
-        if(j<k){B <- B-sum(states*fb[i,j+1,,t])}
+        if(j<k2){B <- B-sum(states*fb[i,j+1,,t])}
         if(j>1){B <- B-sum(states*fb[i,j-1,,t])}
 
         this.call <- call
@@ -355,8 +366,10 @@ approx.lattice.m.hat.em <- function(call, h=0.5){
 ## if s.free is FALSE, then we assume that s is constant over the range
 ## Otherwise it is free to vary. 
 iterate.approx.mle <- function(f, initial.s=0, h=0.5, max.iters=100, eps=0.001, s.free=TRUE){
-  k <- dim(f)[1]
-  if(!is.matrix(initial.s)){initial.s <- matrix(initial.s,k,k)}
+  k1 <- dim(f)[1]
+  k2 <- dim(f)[2]
+
+  if(!is.matrix(initial.s)){initial.s <- matrix(initial.s,k1,k2)}
   M.hat <- approx.lattice.m.hat(f, initial.s, h=h)
   if(s.free){s.hat <- approx.lattice.s.hat(f, M.hat, h=h)}
   else{s.hat <- approx.lattice.s.hat.constant(f, M.hat, h=h)}
@@ -430,7 +443,8 @@ s.m.estimate.em.lattice <- function( obs, Ne, M=NULL, h=0.5, update="Simple", to
   if(!(update %in% c("Simple", "Hard EM", "Stochastic EM", "Soft EM"))){stop("Unknown update rule")}
   
   fixed.M <- is.numeric(M)
-  k <- dim(obs$N)[1]
+  k1 <- dim(obs$N)[1]
+  k2 <- dim(obs$N)[2]
   g <- dim(obs$N)[3]
   f <- interpolate.f(obs)               #Initial starting point: Linearly interpolate. 
   paths <- NULL
@@ -484,16 +498,16 @@ s.m.estimate.em.lattice <- function( obs, Ne, M=NULL, h=0.5, update="Simple", to
       f <- apply(paths,c(1,2,3),mean)
       ## get M hat and s.hat for each path
       M.hats <- rep(0,params$n.paths)
-      s.hats <- array(0,dim=c(k,k,params$n.paths))
+      s.hats <- array(0,dim=c(k1,k2,params$n.paths))
       for(i in 1:params$n.paths){
         this.s.m <- update.s.m(paths[,,,i], fixed.M, h=h, M=M, s.free=s.free)
         M.hats[i] <- this.s.m$M
         s.hats[,,i] <- this.s.m$s
       }
       ## Should be able to do this with apply? 
-      this.s <- matrix(0,k,k)
-      for(ii in 1:k){
-        for(jj in 1:k){
+      this.s <- matrix(0,k1,k2)
+      for(ii in 1:k1){
+        for(jj in 1:k2){
           this.s[ii,jj] <- mean(s.hats[ii,jj,])
         }
       }
